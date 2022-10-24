337a024943934b8bbab79f8f9f02ba8d29d2ecbd
kim-0617 <kkk5993@naver.com>
2022-10-24 18:07:04 +0900

    2022-10-24

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        리엑트 시작입니다.
+        리엑트 시작입니다!!
       </header>
     </div>
   );
