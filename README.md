0a644ceaa150620275b6da3d4c26e2efdb52f3e0
kim-0617 <kkk5993@naver.com>
2022-10-25 09:57:28 +0900

    22.10.25

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Getting Started with Create React App
 
 ## 리엑트 파일 생성
-npx create-react-app react 01
+npx create-react-app react01
--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        리엑트 시작입니다!!
-      </header>
+      <header className="App-header">리엑트 시작입니다</header>
     </div>
   );
 }
