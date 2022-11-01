309bccb96f39958eccaf9fbc343bfb1e930f60c3
kim-0617 <kkk5993@naver.com>
2022-11-01 18:27:06 +0900

    22.11.01

--- src/components/section/Slider.jsx
+++ src/components/section/Slider.jsx
@@ -11,6 +11,32 @@ import "swiper/css/pagination";
 // import required modules
 import { Autoplay, Navigation, Pagination } from "swiper";
 
+const data = [
+    {
+        small: "ARTIST",
+        big: "COLOR'S POWER",
+        desc: "색은 많은 의미를 내포하고 있습니다. \n 함께 배워보시죠!",
+        btn1: "자세히 보기",
+        btn2: "정보 보기",
+    },
+
+    {
+        small: "ARTIST",
+        big: "COLOR'S POWER",
+        desc: "색은 많은 의미를 내포하고 있습니다. \n 함께 배워보시죠!",
+        btn1: "자세히 보기",
+        btn2: "정보 보기",
+    },
+
+    {
+        small: "ARTIST",
+        big: "COLOR'S POWER",
+        desc: "색은 많은 의미를 내포하고 있습니다. \n 함께 배워보시죠!",
+        btn1: "자세히 보기",
+        btn2: "정보 보기",
+    },
+];
+
 function Slider2({ fonts }) {
     return (
         <section className={`slider__inner ${fonts}`}>
@@ -18,47 +44,47 @@ function Slider2({ fonts }) {
                 pagination={true}
                 navigation={true}
                 autoplay={{
-                    delay: 1000,
+                    delay: 2000,
                     disableOnInteraction: false,
                 }}
                 modules={[Pagination, Navigation, Autoplay]}
                 className="mySwiper">
                 <SwiperSlide>
                     <div className="desc">
-                        <span>ARTIST</span>
-                        <h3>COLOR'S POWER</h3>
+                        <span>{data[0].small}</span>
+                        <h3>{data[0].big}</h3>
                         <p>
-                            색은 많은 의미를 내포하고 있습니다. <br /> 함께 배워보시죠!
+                            {data[0].desc}
                         </p>
                         <div className="btn">
-                            <a href="/">자세히 보기</a>
-                            <a href="/" className="black">정보 보기</a>
+                            <a href="/">{data[0].btn1}</a>
+                            <a href="/" className="black">{data[0].btn1}</a>
                         </div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className="desc">
-                        <span>ARTIST</span>
-                        <h3>COLOR'S POWER</h3>
+                        <span>{data[1].small}</span>
+                        <h3>{data[1].big}</h3>
                         <p>
-                            색은 많은 의미를 내포하고 있습니다. <br /> 함께 배워보시죠!
+                            {data[1].desc}
                         </p>
                         <div className="btn">
-                            <a href="/">자세히 보기</a>
-                            <a href="/" className="black">정보 보기</a>
+                            <a href="/">{data[1].btn1}</a>
+                            <a href="/" className="black">{data[1].btn1}</a>
                         </div>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className="desc">
-                        <span>ARTIST</span>
-                        <h3>COLOR'S POWER</h3>
+                        <span>{data[2].small}</span>
+                        <h3>{data[2].big}</h3>
                         <p>
-                            색은 많은 의미를 내포하고 있습니다. <br /> 함께 배워보시죠!
+                            {data[2].desc}
                         </p>
                         <div className="btn">
-                            <a href="/">자세히 보기</a>
-                            <a href="/" className="black">정보 보기</a>
+                            <a href="/">{data[2].btn1}</a>
+                            <a href="/" className="black">{data[2].btn1}</a>
                         </div>
                     </div>
                 </SwiperSlide>
